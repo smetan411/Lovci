52f4febf77c7a006f582996b9723a64c2ead6e87
BuildTools <unconfigured@null.spigotmc.org>
2022-07-25 17:38:56 +0200

    Initial commit

---
+++ src/main/java/lovci/PruchodPortalem.java
@@ -0,0 +1,24 @@
+package lovci;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerTeleportEvent;
+
+public class PruchodPortalem implements Listener {
+
+    private final StavHry stavHry;
+
+    public PruchodPortalem(StavHry stavHry) {
+        this.stavHry = stavHry;
+    }
+
+    @EventHandler
+    public void projdiPortalem(PlayerTeleportEvent event){
+        PlayerTeleportEvent.TeleportCause typUdalosti = event.getCause();
+        if(typUdalosti == PlayerTeleportEvent.TeleportCause.NETHER_PORTAL){
+            stavHry.stop();
+            stavHry.zpravaBezci("Utekls jim, vyhrals. Konec hry.");
+            stavHry.zpravaLovcum("Zdrhl vam, prohrali jste. Konec hry.");
+        }
+    }
+}
---
+++ src/main/java/lovci/StavHry.java
@@ -0,0 +1,45 @@
+package lovci;
+
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public class StavHry {
+    private Player bezec;
+    private List<Player> lovci;
+    private boolean hraJede = false;
+
+    public void start(){
+        hraJede = true;
+    }
+
+    public void stop(){
+        hraJede = false;
+    }
+
+    public boolean jedeHra(){
+        return hraJede;
+    }
+
+    public void setBezec(Player bezec, List<Player> lovci){
+        this.bezec = bezec;
+        this.lovci = lovci;
+    }
+
+    public Player getBezec(){
+        return bezec;
+    }
+
+    public List<Player> getLovci(){
+        return lovci;
+    }
+
+    public void zpravaLovcum(String zprava){
+        lovci.forEach(lovec -> lovec.sendMessage(zprava));
+    }
+
+    public void zpravaBezci(String zprava){
+        bezec.sendMessage(zprava);
+    }
+
+}
